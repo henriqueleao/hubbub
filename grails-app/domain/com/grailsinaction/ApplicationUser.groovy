e218a96b215fc339073b71561ae02fd926e2157a
henriqueb <henrique.saraiva@gmail.com>
2016-04-27 08:43:30 -0300

    Initial commit

---
+++ grails-app/domain/com/grailsinaction/ApplicationUser.groovy
@@ -0,0 +1,13 @@
+package com.grailsinaction
+
+class ApplicationUser {
+    String applicationName
+    String password
+    String apiKey
+
+    static constraints = {
+        importFrom User, include: ['password']
+        applicationName blank: false, unique: true
+        apiKey blank: false
+    }
+}
---
+++ grails-app/domain/com/grailsinaction/User.groovy
@@ -0,0 +1,25 @@
+package com.grailsinaction
+
+class User {
+    String loginId
+    String password
+    Date dateCreated
+
+    static hasOne = [profile: Profile]
+    static hasMany = [posts : Post, tags: Tag, following: User]
+
+
+    static constraints = {
+        loginId size: 3..20, unique: true, nullable: false
+        password size: 6..8, blank: false, validator: { passwd, user ->
+            passwd != user.loginId
+        }
+        tags()
+        posts()
+        profile nullable: true
+    }
+
+    static mapping = {
+        posts sort: "dateCreated", order: "desc"
+    }
+}
